43b06cf4fef051619c54d05f2160a71452206a5f
sumancrest0001 <suman.crest0001@gmail.com>
2020-10-21 22:22:27 +0545

    add headerwithicons component

--- src/Components/HeaderWithIcons/HeaderWithIcons.js
+++ src/Components/HeaderWithIcons/HeaderWithIcons.js
@@ -1,16 +1,16 @@
 import React from 'react';
-import RefreshIcon from '../../images/refresh.svg';
-import UncheckedIcon from '../../images/unchecked.svg';
-import DropdownIcon from '../../images/down-arrow.svg';
+import { ReactComponent as RefreshIcon } from '../../images/refresh.svg';
+import { ReactComponent as UncheckedIcon } from '../../images/unchecked.svg';
+import { ReactComponent as DropdownIcon } from '../../images/down-arrow.svg';
 import './HeaderWithIcons.scss';
 
 const headerWithIcons = (props) => (
   <div className="header-with-icons">
     <h2 className="header__title">{props.children}</h2>
     <div className="header__icons icons">
-      <img src={RefreshIcon} alt="refresh icon" className="icons__refresh" />
-      <img src={UncheckedIcon} alt="unchecked icon" className="icons__unchecked" />
-      <img src={DropdownIcon} alt="dropdown icon" className="icons__dropdown" />
+      <RefreshIcon alt="refresh icon" className="icons__refresh" />
+      <UncheckedIcon alt="unchecked icon" className="icons__unchecked" />
+      <DropdownIcon alt="dropdown icon" className="icons__dropdown" />
     </div>
   </div>
 );
