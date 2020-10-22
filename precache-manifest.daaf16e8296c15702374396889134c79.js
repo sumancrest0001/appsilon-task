5542ceab06cd5740de1b34df8421944e54c33b96
sumancrest0001 <suman.crest0001@gmail.com>
2020-10-23 01:44:23 +0545

    Updates

---
+++ precache-manifest.daaf16e8296c15702374396889134c79.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b96a465998d365716369e6623d016ad",
+    "url": "/appsilon-task/index.html"
+  },
+  {
+    "revision": "1ffead1184ba37f2a8ec",
+    "url": "/appsilon-task/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "62f11c67903a7d28039b",
+    "url": "/appsilon-task/static/css/main.72420166.chunk.css"
+  },
+  {
+    "revision": "1ffead1184ba37f2a8ec",
+    "url": "/appsilon-task/static/js/2.849221a4.chunk.js"
+  },
+  {
+    "revision": "1d9ed9265a32ea4776472f445cf2df78",
+    "url": "/appsilon-task/static/js/2.849221a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62f11c67903a7d28039b",
+    "url": "/appsilon-task/static/js/main.f8f08a0b.chunk.js"
+  },
+  {
+    "revision": "096bf5d567ba0e0f29c4",
+    "url": "/appsilon-task/static/js/runtime-main.a70ade40.js"
+  },
+  {
+    "revision": "7be6c5aed527bbd935a92400ab80bf3b",
+    "url": "/appsilon-task/static/media/bar-chart.7be6c5ae.png"
+  },
+  {
+    "revision": "67de72cdc84fb77311279b8b9c01f9b3",
+    "url": "/appsilon-task/static/media/countries.67de72cd.png"
+  },
+  {
+    "revision": "a21052b62707bb44f2dd12103127d11f",
+    "url": "/appsilon-task/static/media/database.a21052b6.svg"
+  },
+  {
+    "revision": "47fb15ce6eb8bea622e648492898e1bb",
+    "url": "/appsilon-task/static/media/down-arrow.47fb15ce.svg"
+  },
+  {
+    "revision": "e8c4daf7a2fd42bfea21cb9b7a24a18c",
+    "url": "/appsilon-task/static/media/download.e8c4daf7.svg"
+  },
+  {
+    "revision": "8a645233de9204c932dadccb725cc07c",
+    "url": "/appsilon-task/static/media/more.8a645233.svg"
+  },
+  {
+    "revision": "32be12ebcd74fac97581a3c96dd970e5",
+    "url": "/appsilon-task/static/media/printer.32be12eb.svg"
+  },
+  {
+    "revision": "bd0c2844aa9c6bb906a70522c2f1dd64",
+    "url": "/appsilon-task/static/media/refresh.bd0c2844.svg"
+  },
+  {
+    "revision": "4ac9ab46a0801fcc31f67ef69fd879db",
+    "url": "/appsilon-task/static/media/unchecked.4ac9ab46.svg"
+  },
+  {
+    "revision": "625b6e018904b734004cd2e5187f2a41",
+    "url": "/appsilon-task/static/media/user-profile.625b6e01.svg"
+  },
+  {
+    "revision": "614e6e48390f840302ebb500f17a78e8",
+    "url": "/appsilon-task/static/media/verticalDots.614e6e48.svg"
+  }
+]);
